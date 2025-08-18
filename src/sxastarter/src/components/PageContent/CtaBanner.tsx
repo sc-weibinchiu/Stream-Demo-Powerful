9c15781af70d85aeb08ead01877d363373d219ca
Wei Bin Chiu <weibin.chiu@sitecore.com>
2025-08-18 18:26:43 +0800

    Update CtaBanner.tsx

--- src/sxastarter/src/components/PageContent/CtaBanner.tsx
+++ src/sxastarter/src/components/PageContent/CtaBanner.tsx
@@ -23,7 +23,7 @@ interface Fields {
   Image: ImageField;
   Icon: ImageField;
 }
-//wb 123456
+//wb 1234567
 export type CtaBannerProps = ComponentProps & {
   params: { [key: string]: string };
   fields: Fields;
