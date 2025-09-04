911870bc726f9f77651d5ccd5bb44d8783ed6a6a
sc-chiuweibin <weibin.chiu@sitecore.com>
2025-09-04 10:51:43 +0800

    Update test comment in DocumentsList.tsx

--- src/sxastarter/src/components/PageContent/DocumentsList.tsx
+++ src/sxastarter/src/components/PageContent/DocumentsList.tsx
@@ -30,7 +30,7 @@ export type DocumentsListProps = {
   params: { [key: string]: string };
   fields: Fields;
 };
-//test 1234567
+//test 12345678
 const ComponentDefault = (props: DocumentsListProps): JSX.Element => (
   <div className={`component ${props.params?.styles}`.trimEnd()}>
     <div className="component-content">
