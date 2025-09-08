0ad393e75cd2a93839fa7a2278ec8859995629d4
sc-chiuweibin <weibin.chiu@sitecore.com>
2025-09-08 15:15:57 +0800

    Update version comment in Accordion.tsx

--- src/sxastarter/src/components/PageContent/Accordion.tsx
+++ src/sxastarter/src/components/PageContent/Accordion.tsx
@@ -12,7 +12,7 @@ export type AccordionItemProps = {
   name: string;
   url: string;
 };
-//new-tenant-wb1-1-2-3
+//new-tenant-wb1-1-2-3-4
 export type AccordionProps = {
   params: { [key: string]: string };
   fields: {
